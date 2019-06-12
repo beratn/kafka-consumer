987861bcf9512a0efa76b260a0f02f6b9037ec04
Berat <beratnakip@gmail.com>
2019-06-12 23:11:34 +0300

    adding some classes

--- src/main/java/com/teb/consumer/App.java
+++ src/main/java/com/teb/consumer/App.java
@@ -1,28 +1,15 @@
 package com.teb.consumer;
 
 import com.teb.consumer.Consumer.ConsumerThread;
-import com.teb.consumer.Factory.DataViewerFactory;
 
 public class App {
 
     public static void main(String[] args) {
-        try {
-//            ConsumerThread consumerRunnable = new ConsumerThread();
-//            consumerRunnable.run();
-            runChart();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        ConsumerThread consumerRunnable = new ConsumerThread();
+        consumerRunnable.run();
+
     }
 
-    private static void runChart() throws InterruptedException {
 
-        DataViewerFactory.createDataViewerExample1(); // http://localhost:8090/view/example1
-
-//		 http://localhost:8090/view/updateExample
-//        DataViewerFactory.createDataViewerUpdateExample();
-
-        Thread.currentThread().join();
-    }
 
 }
